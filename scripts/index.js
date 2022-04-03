008f94fd6a3966f57171b8ebd8f9a384129c3624
Ilya Bikmetov <ilya-bikmetov@yandex.ru>
2022-04-03 21:42:47 +0700

    hotfix/review1

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,8 @@ const editButtonLink = document.querySelector('.profile__edit-button');
 const elementPopup = document.querySelector('.popup');
 const closeButtonLink = document.querySelector('.popup__close');
 const elementLikeList = document.querySelectorAll('.elements__like');
-const saveButtonLink= document.querySelector('.popup__save');
+const saveButtonLink = document.querySelector('.popup__save');
+const elementBody = document.querySelector('.root');
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
 let nameInput = document.querySelector('.popup__name');
@@ -13,6 +14,7 @@ function popupAddClass() {
   elementPopup.classList.toggle('popup_active');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
+  elementBody.classList.toggle('root_scroll');
 }
 
 editButtonLink.addEventListener('click', popupAddClass);
