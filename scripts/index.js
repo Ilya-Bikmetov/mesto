82679a3c728de91c72a921c73ec94ddca35caf13
Ilya Bikmetov <ilya-bikmetov@yandex.ru>
2022-04-10 21:41:30 +0700

    feat/cards

--- scripts/index.js
+++ scripts/index.js
@@ -35,3 +35,50 @@ function formSubmitHandler(evt) {
 
 formElement.addEventListener('submit', formSubmitHandler);
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const templateCard = document.querySelector('.templateCard');
+const elementsList = document.querySelector('.elements__list');
+
+function placeCard() {
+  const html = initialCards.map(getElement);
+  elementsList.append(...html);
+}
+
+function getElement(item) {
+  const getElementTemplateCard = templateCard.content.cloneNode(true);
+  const title = getElementTemplateCard.querySelector('.elements__title');
+  const img = getElementTemplateCard.querySelector('.elements__photo');
+  img.src = item.link;
+  img.alt = item.name;
+  title.textContent = item.name;
+  return getElementTemplateCard;
+}
+
+placeCard();
+
